e105851668cc7b0691f75de7dd96ca3eb993d3eb
xySaad <saadrm.dev@gmail.com>
2025-03-28 21:42:20 +0000

    fix: nested When conditions

--- src/core/native.js
+++ src/core/native.js
@@ -1,11 +1,8 @@
-import { isReference, useReference } from "./reference.js";
+import { isReference } from "./reference.js";
 export const _ = null;
-class Null {
-  constructor() {}
-}
 
 const append = function (...children) {
-  for (const child of children) {
+  for (const child of children) {    
     Element.prototype.append.call(this.active, child.active);
   }
   return this;
@@ -84,110 +81,3 @@ class EventListener {
 export const onclick = (callback) => new EventListener(callback, "onclick");
 export const oninput = (callback) => new EventListener(callback, "oninput");
 export const onchange = (callback) => new EventListener(callback, "onchange");
-
-// Conditional rendering
-export const useDependency = (dependency, resolver) => {
-  const newRefernce = useReference();
-  dependency.addTrigger((value) => newRefernce(resolver(value)));
-  return newRefernce;
-};
-
-export const When = (reference, condition) => {
-  const isTruthy = (value) => (condition && condition(value)) || value;
-
-  let element = {};
-
-  return {
-    show: (trueElement) => {
-      reference.addTrigger((value) => {
-        if (isTruthy(value)) {
-          element.active?.replaceWith(trueElement.active);
-          element.active = trueElement.active;
-        }
-      });
-
-      return {
-        else: (falseElement) => {
-          reference.addTrigger((value) => {
-            if (isTruthy(value)) {
-            } else {
-              element.active?.replaceWith(falseElement.active);
-              element.active = falseElement.active;
-            }
-          });
-
-          return element;
-        },
-      };
-    },
-    do: (trueValue, falseValue) => {
-      return useDependency(reference, (v) => v?trueValue: falseValue);
-    },
-  };
-};
-
-// this function maybe useful in case of using a tanspiler like Babel to convert JSX
-// NOTE: this will result in rendering the inner condition each time the outer condition got rendered even if the inner condition dependencies aren't changed
-export const WhenFunctionBased = (reference) => {
-  let element = {};
-  return (resolver) => {
-    console.log(resolver.toString());
-    reference.addTrigger((value) => {
-      const resolvedElement = resolver(value);
-      element.active?.replaceWith(resolvedElement.active);
-      element.active = resolvedElement.active;
-    });
-    return resolver(reference());
-  };
-};
-// example
-// When(isLoading)((v) =>
-//   v
-//     ? div("loading", "loading...")
-//     : When(pairCode)((v) =>
-//         v ? div("pairCode", pairCode) : phoneInput
-//       )
-// ),
-///////////////////////////
-
-// Loading state
-export const useLoading = (asynFunc) => {
-  let _loadingElement = null;
-  let _resolveElement = null;
-  let _fallbackElement = null;
-
-  const fn = (...args) => {
-    _resolveElement.active.replaceWith(_loadingElement.active);
-    _fallbackElement?.active.replaceWith(_loadingElement.active);
-
-    const result = asynFunc(...args);
-
-    result.catch(() => {
-      _loadingElement.active.replaceWith(_fallbackElement?.active);
-    });
-
-    result.then(() => {
-      _loadingElement.active.replaceWith(_resolveElement.active);
-    });
-
-    return result;
-  };
-
-  const loadingFn = (loadingElement) => {
-    _loadingElement = loadingElement;
-
-    _loadingElement.then = (resolveElement) => {
-      _resolveElement = resolveElement;
-      _resolveElement.catch = (fallbackElement) => {
-        _fallbackElement = fallbackElement;
-        return _resolveElement;
-      };
-
-      return _resolveElement;
-    };
-
-    return _loadingElement;
-  };
-
-  return [fn, loadingFn];
-};
