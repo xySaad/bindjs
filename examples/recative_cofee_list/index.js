e1b161e88741f01f35975d0f02d513093eda4736
xySaad <saadrm.dev@gmail.com>
2025-06-06 15:43:09 +0100

    fix: adjust examples

--- examples/recative_cofee_list/index.js
+++ examples/recative_cofee_list/index.js
@@ -1,40 +1,33 @@
-import { When } from "../../src/core/conditional.js";
-import { div, query, select, option } from "../../src/core/native.js";
 import { useReference } from "../../src/core/reference.js";
+import htmlElements from "../../src/html/index.js";
+import { query } from "../../src/html/native.js";
+const { div, select, option } = htmlElements;
+
+const getCoffeeList = (coffeeData) => async (e) => {
+  const coffeeType = e.target.value;
+  if (!coffeeType) return coffeeData([]);
+  const resp = await fetch(`https://api.sampleapis.com/coffee/${coffeeType}`);
+  coffeeData(await resp.json());
+};
 
 const coffeeOptions = (...options) => {
   const coffeeData = useReference([]);
-
-  const handleListChange = async (e) => {
-    const coffeeType = e.target.value;
-    if (!coffeeType) return coffeeData([]);
-    const resp = await fetch(`https://api.sampleapis.com/coffee/${coffeeType}`);
-    coffeeData(await resp.json());
-  };
-
-  return div("container").add(
-    select("", { onchange: handleListChange }).add(...options),
-    When(coffeeData, (v) => {
-      const coffee = v[0]
-      return div("coffee").add(
-        div("title", coffee?.title),
-        div("desc", coffee?.description)
-      );
-    })
+  return div().add(
+    select({ onchange: getCoffeeList(coffeeData) }).add(...options),
+    coffeeData.map((coffee) =>
+      div({ class: "coffee" }).add(
+        div({ class: "title", textContent: coffee.title }),
+        div({ class: "desc", textContent: coffee.description })
+      )
+    )
   );
 };
 
-// coffeeData.map((coffee) =>
-//   div("coffee").add(div("title", coffee.title), div("desc", coffee.description))
-// );
-
 query("app").append(
-  div("parent").add(
-    div("some class", "hello world!"),
-    coffeeOptions(
-      option("", "Please choose coffee type"),
-      option("iced", "Iced Coffee"),
-      option("hot", "Hot Coffee")
-    )
+  div({ class: "some class", textContent: "hello world!" }),
+  coffeeOptions(
+    option({ value: "", textContent: "Please choose coffee type" }),
+    option({ value: "iced", textContent: "Iced Coffee" }),
+    option({ value: "hot", textContent: "Hot Coffee" })
   )
 );
