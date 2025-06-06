b5a12352defb8592a7a0dceb398615ef7e34a738
xySaad <saadrm.dev@gmail.com>
2025-06-06 15:29:53 +0100

    feat: add eslint (avoid import cycle)

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint/config";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
+
+export default defineConfig([{
+    plugins: {
+        import: fixupPluginRules(_import),
+    },
+
+    rules: {
+        "import/no-cycle": ["error", {
+            maxDepth: 10,
+        }],
+    },
+}]);
