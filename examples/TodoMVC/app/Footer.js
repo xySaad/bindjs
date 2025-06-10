963756618f498628034692f00e4cea03b9e398d3
xySaad <saadrm.dev@gmail.com>
2025-06-10 16:57:50 +0100

    fix: prevent footer re-rendering on each todos state change

--- examples/TodoMVC/app/Footer.js
+++ examples/TodoMVC/app/Footer.js
@@ -2,7 +2,7 @@ import htmlElements from "../../src/html/index.js";
 import { frag } from "../../src/html/custom/fragment.js";
 import { Link } from "../../../src/router/Link.js";
 import { deleteCompleted, todos } from "./context/todos.js";
-import { When } from "../../../src/core/conditional.js";
+import { If, When } from "../../../src/core/conditional.js";
 
 const { footer, span, strong, ul, div, button } = htmlElements;
 
@@ -10,34 +10,31 @@ const isSelected = (path) => (location.pathname === path ? "selected" : "");
 
 export const Footer = () => {
   const active = todos.filterRef((t) => !t.completed);
-  
-  return When((w) => {
-    return (
-      w(todos).length > 0 &&
-      footer({ class: "footer" }).add(
-        frag(
-          span({ class: "todo-count" }).add(
-            strong({
-              textContent: [
-                active.len(),
-                active.len() === 1 ? "item" : "items",
-                "left!",
-              ],
-            })
-          ),
-          ul({ class: "filters" }).add(
-            Link("/", "All", isSelected("/")),
-            Link("/active", "Active", isSelected("/active")),
-            Link("/completed", "Completed", isSelected("/completed"))
-          ),
-          button({
-            class: "clear-completed",
-            textContent: "Clear Completed",
-            hidden: todos.every((t) => !t.completed),
-            onclick: deleteCompleted,
+  return If(
+    todos.len(),
+    footer({ class: "footer" }).add(
+      frag(
+        span({ class: "todo-count" }).add(
+          strong({
+            textContent: [
+              active.len(),
+              active.len() === 1 ? "item" : "items",
+              "left!",
+            ],
           })
-        )
+        ),
+        ul({ class: "filters" }).add(
+          Link("/", "All", isSelected("/")),
+          Link("/active", "Active", isSelected("/active")),
+          Link("/completed", "Completed", isSelected("/completed"))
+        ),
+        button({
+          class: "clear-completed",
+          textContent: "Clear Completed",
+          hidden: todos.every((t) => !t.completed),
+          onclick: deleteCompleted,
+        })
       )
-    );
-  });
+    )
+  );
 };
