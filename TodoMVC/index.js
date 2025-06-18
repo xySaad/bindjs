9ab5030b5a61ad929c768db2e1767e9828b2e89b
fahdaguenouz <faguenouz@gmail.com>
2025-06-18 12:06:55 +0100

    seperate html

--- TodoMVC/index.js
+++ TodoMVC/index.js
@@ -9,31 +9,11 @@ import {
   label,
   ul,
 } from "../src/native.js";
+import { CreatHTML } from "./html.js";
 
 const App = () => {
-  return section({ className: "todoapp", id: "root" }).add(
-    header({ className: "header", "date-testid": "header" }).add(
-      h1({ textContent: "todos" }),
-      div({ className: "input-container" }).add(
-        input({
-          className: "new-todo",
-          id: "todo-input",
-          type: "text",
-          "data-testid": "text-input",
-          placeholder: "What needs to be done?",
-        }),
-        label({
-          className: "visually-hidden",
-          for: "todo-input",
-          textContent: "New Todo Input",
-        })
-      )
-    ),
-    main({ className: "main", "date-testid": "main" }).add(
-      ul({ className: "todo-list", "data-testid": "todo-list" })
-    ),
-    footer({ className: "footer", "date-testid": "footer" })
-  );
+  
+  return CreatHTML()
 };
 
 document.body.append(App());
